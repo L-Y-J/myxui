98723b1a92ec806804da0739fbfd641de4ab61b9
L-Y-J <LYJ_LYJ@126.com>
2015-04-03 20:36:35 +0800

    improve ioc to support map node

--- src/main/java/tinyioc/beans/xml/XmlBeanDefinitionReader.java
+++ src/main/java/tinyioc/beans/xml/XmlBeanDefinitionReader.java
@@ -13,6 +13,8 @@ import tinyioc.beans.io.ResourceLoader;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author yihua.huang@dianping.com
@@ -55,6 +57,10 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
 		}
 	}
 
+	/**
+	 * 解析bean节点
+	 * @param ele bean节点
+	 */
 	protected void processBeanDefinition(Element ele) {
 		String name = ele.getAttribute("name");
 		String className = ele.getAttribute("class");
@@ -64,6 +70,11 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
 		getRegistry().put(name, beanDefinition);
 	}
 
+	/**
+	 * 解析property节点
+	 * @param ele property节点
+	 * @param beanDefinition 保存节点信息
+	 */
 	private void processProperty(Element ele, BeanDefinition beanDefinition) {
 		NodeList propertyNode = ele.getElementsByTagName("property");
 		for (int i = 0; i < propertyNode.getLength(); i++) {
@@ -72,16 +83,58 @@ public class XmlBeanDefinitionReader extends AbstractBeanDefinitionReader {
 				Element propertyEle = (Element) node;
 				String name = propertyEle.getAttribute("name");
 				String value = propertyEle.getAttribute("value");
+				String ref = propertyEle.getAttribute("ref");
 				if (value != null && value.length() > 0) {
 					beanDefinition.getPropertyValues().addPropertyValue(new PropertyValue(name, value));
-				} else {
-					String ref = propertyEle.getAttribute("ref");
-					if (ref == null || ref.length() == 0) {
-						throw new IllegalArgumentException("Configuration problem: <property> element for property '"
-								+ name + "' must specify a ref or value");
-					}
+				}
+				else if (ref != null && ref.length() > 0){
 					BeanReference beanReference = new BeanReference(ref);
 					beanDefinition.getPropertyValues().addPropertyValue(new PropertyValue(name, beanReference));
+				}
+				else
+					proccessPropertyChilds(propertyEle, beanDefinition);
+			}
+		}
+	}
+
+	/**
+	 * 解析property的孩子节点，例如List，Map等
+	 * @param element property节点
+	 * @param beanDefinition 保存节点信息
+	 */
+	private void proccessPropertyChilds(Element element, BeanDefinition beanDefinition) {
+		NodeList nodeList = element.getChildNodes();         // 第一群孩子
+		String propertyName = element.getAttribute("name");
+		if (nodeList == null || nodeList.getLength() < 1)
+			throw new IllegalArgumentException("configuration file error(property node have no child)");
+		for (int i = 0; i < nodeList.getLength(); i++){       // 进入property孩子节点
+			Node node = nodeList.item(i);
+			if (node instanceof Element){
+				Element ele = (Element) node;
+				if (ele.getTagName().equals("map")){          // 进入map节点
+					NodeList entryNodes = ele.getChildNodes();       // 第二群孩子
+					if (entryNodes == null || entryNodes.getLength() < 1)
+						throw new IllegalArgumentException("configuration file error(map node have no child)");
+					Map map = new HashMap();
+					for (int j = 0; j<entryNodes.getLength(); j++){   // 进入entry节点
+						Node entryNode = entryNodes.item(j);
+						if (entryNode instanceof Element){
+							Element entryEle = (Element) entryNode;
+							NodeList valueNodes = entryEle.getChildNodes();    // 第三群孩子
+							if (valueNodes == null || valueNodes.getLength() < 1)
+								throw new IllegalArgumentException("configuration file error(entry node have no child)");
+							for (int k = 0; k < valueNodes.getLength(); k++){       // 进入value节点
+								Node valueNode = valueNodes.item(k);
+								if (valueNode instanceof Element){
+									Element valueEle = (Element) valueNode;
+									String key = entryEle.getAttribute("key");
+									String value = valueEle.getFirstChild().getNodeValue();
+									map.put(key, value);
+								}
+							}
+						}
+					}
+					beanDefinition.getPropertyValues().addPropertyValue(new PropertyValue(propertyName, map));
 				}
 			}
 		}
