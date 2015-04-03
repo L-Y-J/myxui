3d40e97aeb8b942844c54d62b100e59348881d1d
L-Y-J <LYJ_LYJ@126.com>
2015-04-03 15:40:46 +0800

    imporve ioc(2015-4-3)

--- src/main/java/tinyioc/beans/factory/AutowireCapableBeanFactory.java
+++ src/main/java/tinyioc/beans/factory/AutowireCapableBeanFactory.java
@@ -5,6 +5,7 @@ import tinyioc.beans.BeanDefinition;
 import tinyioc.beans.PropertyValue;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * 可自动装配内容的BeanFactory
@@ -17,7 +18,8 @@ public class AutowireCapableBeanFactory extends AbstractBeanFactory {
 	protected Object doCreateBean(BeanDefinition beanDefinition) throws Exception {
 		Object bean = createBeanInstance(beanDefinition);
         beanDefinition.setBean(bean);
-		applyPropertyValues(bean, beanDefinition);
+		// applyPropertyValues(bean, beanDefinition);
+		applyPropertyValuesByMethod(bean, beanDefinition);
 		return bean;
 	}
 
@@ -25,6 +27,12 @@ public class AutowireCapableBeanFactory extends AbstractBeanFactory {
 		return beanDefinition.getBeanClass().newInstance();
 	}
 
+	/**
+	 * 直接设置bean的属性值
+	 * param bean
+	 * @param mbd
+	 * @throws Exception
+	 */
 	protected void applyPropertyValues(Object bean, BeanDefinition mbd) throws Exception {
 		for (PropertyValue propertyValue : mbd.getPropertyValues().getPropertyValues()) {
 			Field declaredField = bean.getClass().getDeclaredField(propertyValue.getName());
@@ -34,7 +42,19 @@ public class AutowireCapableBeanFactory extends AbstractBeanFactory {
 				BeanReference beanReference = (BeanReference) value;
 				value = getBean(beanReference.getName());
 			}
+			if (declaredField.getType().toString().equals("int")){
+				value = Integer.parseInt(value.toString());
+			}
 			declaredField.set(bean, value);
+		}
+	}
+
+	protected void applyPropertyValuesByMethod(Object bean, BeanDefinition mbd) throws Exception {
+		for (PropertyValue propertyValue : mbd.getPropertyValues().getPropertyValues()) {
+			Field declaredField = bean.getClass().getDeclaredField(propertyValue.getName().toLowerCase());
+			Method method = bean.getClass()
+					.getDeclaredMethod("set" + propertyValue.getName(), new Class[]{declaredField.getType()});
+			method.invoke(bean, new Object[]{propertyValue.getValue()});
 		}
 	}
 }
